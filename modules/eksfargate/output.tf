c6cd9b8657bd58be9cffff0c724af394cffb0634
aniketbadadhe49-devops <aniketbadadhe49@gmail.com>
2025-11-05 00:53:26 +0530

    Create output.tf

---
+++ modules/eksfargate/output.tf
@@ -0,0 +1,44 @@
+output "cluster_id" {
+  description = "EKS cluster ID"
+  value       = aws_eks_cluster.this.id
+}
+
+output "cluster_arn" {
+  description = "EKS cluster ARN"
+  value       = aws_eks_cluster.this.arn
+}
+
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.this.name
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = aws_eks_cluster.this.endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = aws_eks_cluster.this.certificate_authority[0].data
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
+}
+
+output "cluster_iam_role_arn" {
+  description = "IAM role ARN of the EKS cluster"
+  value       = aws_iam_role.cluster.arn
+}
+
+output "fargate_profile_arns" {
+  description = "ARNs of the Fargate profiles"
+  value       = { for k, v in aws_eks_fargate_profile.this : k => v.arn }
+}
+
+output "fargate_pod_execution_role_arn" {
+  description = "ARN of the Fargate pod execution role"
+  value       = aws_iam_role.fargate_pod_execution.arn
+}
