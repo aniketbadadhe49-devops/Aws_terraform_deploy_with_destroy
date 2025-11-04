dfdef6d9b2b00950e95749c20f7c08c89c94282b
aniketbadadhe49-devops <aniketbadadhe49@gmail.com>
2025-11-05 00:36:06 +0530

    Create main.tf

---
+++ envs/dev/main.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+  source = "../../modules/vpc"
+  name   = var.name
+  cidr   = var.vpc_cidr
+  azs    = var.azs
+  public_subnets_cidrs  = var.public_subnets_cidrs
+  private_subnets_cidrs = var.private_subnets_cidrs
+}
